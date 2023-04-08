baf6d084e2613b2840f38d455b13df26b80959f7
uynot <kaho399@gmail.com>
2023-04-09 06:59:19 +0800

    update getCapsuleStockFull method

--- springbootcrudapi/src/main/java/stock/inventory/springbootcrudapi/service/StockServiceImpl.java
+++ springbootcrudapi/src/main/java/stock/inventory/springbootcrudapi/service/StockServiceImpl.java
@@ -22,7 +22,6 @@ public class StockServiceImpl implements StockService{
 		return stockDAO.getStockFull();
 	}
 	
-	@Override
 	public QuantityByItemType getQuantityByItemTypeFull() {
 		return stockDAO.getQuantityByItemTypeFull();
 	}
